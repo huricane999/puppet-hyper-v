43243bb23211d56878fc48af945e758b93ccde31
Chris Smith <casmit14@ncsu.edu>
2017-07-28 16:02:25 -0400

    remove unneeded check

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
 # Class hyperv::install
 class hyperv::install inherits hyperv {
-  if !$::hyperv_conf and $::hyperv::install_feature {
+  if $::hyperv::install_feature {
     exec { "Install ${::hyperv::feature_name}":
       command  => "Install-WindowsFeature -Name '${::hyperv::feature_name}' -IncludeAllSubFeature -IncludeManagementTools",
       unless   => "if((Get-WindowsFeature '${::hyperv::feature_name}').Installed){exit 1}",
