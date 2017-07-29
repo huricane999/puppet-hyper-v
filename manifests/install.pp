a47b0eb331a89772bb540a4b475073010c1d8d1c
Chris Smith <huricane.smithy@gmail.com>
2017-07-28 21:34:14 -0400

    unless exit should be 0

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,7 +3,7 @@ class hyperv::install inherits hyperv {
   if $::hyperv::install_feature {
     exec { "Install ${::hyperv::feature_name}":
       command  => "Install-WindowsFeature -Name '${::hyperv::feature_name}' -IncludeAllSubFeature -IncludeManagementTools",
-      unless   => "if((Get-WindowsFeature '${::hyperv::feature_name}').Installed){exit 1}",
+      unless   => "if((Get-WindowsFeature '${::hyperv::feature_name}').Installed){exit 0}",
       provider => powershell,
     }
   }
